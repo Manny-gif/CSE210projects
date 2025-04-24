c69ab71e276d82fe52aa0467066e887af2b1017e
manny <yasaemmanuel@byui.edu>
2025-04-24 12:10:56 -0600

    readme assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -11,6 +11,9 @@ class Program
         Console.WriteLine("input your last name");
         string lastName = Console.ReadLine();
         Console.WriteLine($"your is:  { lastName}, {firstName} {lastName}");
+        Console.WriteLine("input your age");
+        string age = Console.ReadLine();
+        Console.WriteLine($"your age is:  { age}");
 
     }
 }
