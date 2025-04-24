da878354e4648962a805d3a6de102b0e03ddc176
manny <yasaemmanuel@byui.edu>
2025-04-24 11:13:59 -0600

    first code

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("input yout first name");
+        string firstName = Console.ReadLine();
+        Console.WriteLine($"your first name is:  { firstName}");
+        Console.WriteLine("input your last name");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"your is:  { lastName}, {firstName} {lastName}");
+
     }
 }
